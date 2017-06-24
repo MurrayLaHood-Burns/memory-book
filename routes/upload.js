175aa5dede4e20df0afa1ff5d653a33e55ea93f2
MurrayLaHood-Burns <murray.lahoodburns@gmail.com>
2017-06-24 18:39:47 +0000

    upload route set up

--- routes/upload.js
+++ routes/upload.js
@@ -10,11 +10,42 @@ var debug = require('debug')('memory-book:server');
 var path = require('path');
 var multer = require('multer');
 var upload = multer({
-  dest: path.join(__dirname, '../public/upload/temp')
+  dest: path.join(__dirname, '../public/upload/temp'),
+  fileFilter: function fileFilter (req, file, cb) {
+    var ext = file.originalname.substr(file.originalname.lastIndexOf('.') + 1);
+    
+    // no extension
+    if(ext === file.originalname)
+    {
+      cb(null, false);
+    }
+
+    ext = ext.toLower();
+
+    switch(ext)
+    {
+      case "jpeg":
+      case "jpg":
+      case "gif":
+      case "png":
+        cb(null, true);
+        break;
+      default:
+        cb(null, false);
+        break;
+    }
+  }
 });
 
-/* POST uploads */
-router.get('/', function(req, res) {
+/* POST upload */
+router.post('/', upload.array('photos', 50), function(req, res, next) {
+  if( !req.files || req.files ){
+    return res.status(400).json({message:'No files uploaded. Select files with a \".jpg\", \".jpeg\", \".gif\", or \".png\" extension\n 1 MB filesize limit'});
+  }
+
+
+
 });
 
 module.exports = router;
+
